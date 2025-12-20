0266aa01f34684f8f307d8983f1d35b6290679f7
JUNAIDKAREEM <shaikjunaid1964@gmail.com>
2025-12-20 21:56:03 +0530

    Day 2 - Functions and Lists practice

---
+++ python-basics/Python-Basics/day2.py
@@ -0,0 +1,14 @@
+#function
+def add(a,b):
+  return a+b
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+print("sum:",add(a,b))
+
+#tuple
+tuple=[1,3,4,5]
+print("the tuple is:",tuple)
+
+#append
+tuple.append(10)
+print(tuple)
