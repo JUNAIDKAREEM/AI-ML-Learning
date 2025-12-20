ec36a02039f79584868dff45e8c1cdeaf0f859e8
JUNAIDKAREEM <shaikjunaid1964@gmail.com>
2025-12-20 22:12:35 +0530

    Day 2 - Functions and Lists practice
    Refactor tuple operations and fix method names.

--- Python-Basics/day2.py
+++ Python-Basics/day2.py
@@ -6,9 +6,22 @@ y=int(input("enter a number"))
 print("sum:",add(a,b))
 
 #tuple
-tuple=[1,3,4,5]
-print("the tuple is:",tuple)
+t=[1,3,4,5]
+print("the tuple is:",t)
 
 #append
-tuple.append(10)
-print(tuple)
+t.append(10)
+print(t)
+
+#delete
+t.ddelete(3)
+print(t)
+
+#concatination
+t1=[10,50,70]
+result=t+t1
+print(result)
+
+#remove
+t.remove(10)
+print(t)
